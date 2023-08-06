78b505c384c289008da5e93ced4314cc8b0f9071
kesterlester <cgl20@cam.ac.uk>
2023-08-07 00:31:45 +0100

    Actually opemn the pages

--- opener.sh
+++ opener.sh
@@ -16,7 +16,7 @@ exiftool -c "%.6f" "$1" `# Get GPS coordinates in decimal` | \
 	awk '{print "open https://www.openstreetmap.org/edit#map=19/" $5 $4 "/" $7 $6}' `# print what OSM wants except there is a stupid comma and NSEW instead of signs` | \
 	sed 's/,//g' `# Get rid of a comma` | \
 	tr 'NOWOSOE' '+ - - +' `# Turn the letters NSWE to +--+ The Os are to stop +--+ being interpreted as some kind of control character.` | \
-	cat # open the browser and start editing
+	sh # open the browser and start editing
 
 # https://www.google.com/maps?11=51.096667,0.535556&9=51.096667,0.5355568h1=en&t=m&z=19
 
@@ -26,4 +26,4 @@ exiftool -c "%.6f" "$1" `# Get GPS coordinates in decimal` | \
 	sed 's/,//g' `# Get rid of a comma we don't need` | \
 	sed 's/COMMA/, /g' `# Put in the comma we need` | \
 	tr 'NOWOSOE' '+ - - +' `# Turn the letters NSWE to +--+ The Os are to stop +--+ being interpreted as some kind of control character.` | \
-	cat # open the browser and start editing
+	sh # open the browser and start editing
