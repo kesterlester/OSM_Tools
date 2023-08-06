fa6ebf37abe569b22b06326423e4e4351501314c
kesterlester <cgl20@cam.ac.uk>
2023-08-06 09:47:03 +0100

    Updates

--- opener.sh
+++ opener.sh
@@ -10,18 +10,20 @@
 
 
 exiftool -c "%.6f" "$1" `# Get GPS coordinates in decimal` | \
+    tee 1 | \
 	grep 'GPS Position' | \
+    tee 2 | \
 	awk '{print "open https: //www.openstreetmap.org/edit#map=19/" $5 $4 "/" $7 $6}' `# print what OSM wants except there is a stupid comma and NSEW instead of signs` | \
 	sed 's/,//g' `# Get rid of a comma` | \
 	tr 'NOWOSOE' '+ - - +' `# Turn the letters NSWE to +--+ The Os are to stop +--+ being interpreted as some kind of control character.` | \
-	sh # open the browser and start editing
+	cat # open the browser and start editing
 
 # https://www.google.com/maps?11=51.096667,0.535556&9=51.096667,0.5355568h1=en&t=m&z=19
 
 exiftool -c "%.6f" "$1" `# Get GPS coordinates in decimal` | \
 	grep 'GPS Position' | \
-	awk '{print "open \"https://www.google.com/maps?11=" $5 $4 "COMMA" $7 $6 "&hl=en&t=m&z=19\""}' `# print wh at GOOGLE wants except there is a stupid comma and NSEW instead of signs` | \
+	awk '{print "open \"https://www.google.com/maps?ll=" $5 $4 "COMMA" $7 $6 "&hl=en&t=m&z=19\""}' `# print wh at GOOGLE wants except there is a stupid comma and NSEW instead of signs` | \
 	sed 's/,//g' `# Get rid of a comma we don't need` | \
 	sed 's/COMMA/, /g' `# Put in the comma we need` | \
 	tr 'NOWOSOE' '+ - - +' `# Turn the letters NSWE to +--+ The Os are to stop +--+ being interpreted as some kind of control character.` | \
-	sh # open the browser and start editing
+	cat # open the browser and start editing
