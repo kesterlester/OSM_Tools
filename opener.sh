3c22df9cb897a182809acaddc9a7a30b9b0b269e
kesterlester <cgl20@cam.ac.uk>
2023-08-08 09:54:52 +0100

    Rationalising

--- opener.sh
+++ opener.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Run with a single argument, the name of an image file containing EXIF data. Result, URLs that you can use to edit that location in OSM.
 #
 # e.g.
@@ -16,16 +16,14 @@
 #
 
 
-GPS_COORDS=$(
-  exiftool -c "%.6f" "$1" `# Get GPS coordinates in decimal` | \
+read -r GPS_LAT GPS_LON < <(  exiftool -c "%.6f" "$1" `# Get GPS coordinates in decimal` | \
     grep 'GPS Position' | \
     sed 's/.*://g' `# Get rid of the "GPS Position : " text` | \
     tr 'NOWOSOE,' '+ - - + ' `# Turn the letters NSWE to +--+ and remove a comma we don't want.  The Os are to stop +--+ being interpreted as some kind of control character.` | \
-    awk '{print $2 $1 " " $4 $3}' `# put signs in correct places`
+    awk '{print $2 $1 " " $4 $3}' `# put signs in correct places so as to output LATITUDE followed by LONGITUDE` 
 )
 
-echo $GPS_COORDS | \
-  awk '{print "https://www.openstreetmap.org/edit#map=19/" $1 "/" $2 }' `# print what OSM wants`
+#echo $GPS_LAT MOO $GPS_LON
 
-echo $GPS_COORDS | \
-  awk '{print "https://www.google.com/maps?ll=" $1 "," $2 "&hl=en&t=m&z=19"}' `# print what GOOGLE wants` 
+echo "https://www.openstreetmap.org/edit#map=19/$GPS_LAT/$GPS_LON"
+echo "https://www.google.com/maps?ll=$GPS_LAT,$GPS_LON"'&hl=en&t=m&z=19'
