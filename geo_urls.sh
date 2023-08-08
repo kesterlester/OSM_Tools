8e7f3faf76d8362f9ac789eb390c9f0a0ac4f9ec
kesterlester <cgl20@cam.ac.uk>
2023-08-08 10:24:27 +0100

    Work on an image with a space in the name

--- geo_urls.sh
+++ geo_urls.sh
@@ -14,7 +14,7 @@
 #
 # open `./geo_urls.sh TEST_IMAGES/IMG_6773.jpeg`
 
-IMAGE="$1"
+IMAGE=$1
 
 read -r GPS_LAT GPS_LON < <(  
   exiftool -c "%.6f" "$IMAGE" `# Get GPS coordinates in decimal` | \
@@ -23,13 +23,11 @@ read -r GPS_LAT GPS_LON < <(
     tr 'NOWOSOE,' '+ - - + ' `# Turn the letters NSWE to +--+ and remove a comma we don't want.  The Os are to stop +--+ being interpreted as some kind of control sequence.` | \
     awk '{print $2 $1 " " $4 $3}' `# put signs in correct places so as to output LATITUDE followed by LONGITUDE` 
 )
-
-#echo Latitude and Longitude from $IMAGE are $GPS_LAT MOO $GPS_LON
-
+ 
+echo Latitude and Longitude from $IMAGE are $GPS_LAT MOO $GPS_LON
+  
 GEO_URL_OSM="https://www.openstreetmap.org/edit#map=19/$GPS_LAT/$GPS_LON"
 GEO_URL_GOOGLE_MAPS="https://www.google.com/maps?ll=$GPS_LAT,$GPS_LON"'&hl=en&t=m&z=19'
 
 echo $GEO_URL_OSM
 echo $GEO_URL_GOOGLE_MAPS
-
-
